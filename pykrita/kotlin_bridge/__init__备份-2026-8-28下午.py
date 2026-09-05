b674127e4710985e035fbb15a454868bb8861e62
zzh2024424 <167963169+zzh2024424@users.noreply.github.com>
2026-09-05 22:27:43 +0800

    Delete pykrita/kotlin_bridge/__init__备份-2026-8-28下午.py

--- pykrita/kotlin_bridge/__init__备份-2026-8-28下午.py
+++
@@ -1,6 +0,0 @@
-from krita import Krita
-from .kotlin_bridge import KotlinBridgeExtension
-
-# 将你的扩展类注册到 Krita 实例中
-#Krita.instance().addExtension(KotlinBridgeExtension(Krita.instance()))
-#2026-7-2将第4行注释掉
